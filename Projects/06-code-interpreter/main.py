100edd9729d7bda418b700a8f48a4502d4258894
Venkatraman <adithyavenkat2002@gmail.com>
2025-09-09 17:58:36 +0530

    Started with new project

---
+++ Projects/06-code-interpreter/main.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+from langchain import hub
+from langchain_openai import ChatOpenAI
+from langchain.agents import create_react_agent, AgentExecutor
+from langchain_experimental.tools import PythonREPLTool
+
+load_dotenv()
+
+
+def main():
+    print("main func")
+
+if __name__ == "__main__":
+    main()
