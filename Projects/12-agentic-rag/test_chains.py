b5d8db7631f0c0112a5a6c83f66c077b90de9f67
Venkatraman <adithyavenkat2002@gmail.com>
2025-09-11 16:09:04 +0530

    completed half of the agentic rag project

---
+++ Projects/12-agentic-rag/test_chains.py
@@ -0,0 +1,29 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from retrieve_grader import GradeDocuments, retrieval_grader
+from ingestion import retriever
+
+def test_retrieval_grader_answer_yes() -> None:
+    question = "agency memory"
+    docs = retriever.invoke(question)
+    doc_txt = docs[1].page_content
+
+    res: GradeDocuments = retrieval_grader.invoke(
+        {"question": question, "document": doc_txt}
+    )
+
+    assert res.binary_score == "yes"
+
+
+def test_retrival_grader_answer_no() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+    doc_txt = docs[1].page_content
+
+    res: GradeDocuments = retrieval_grader.invoke(
+        {"question": "how to make pizaa", "document": doc_txt}
+    )
+
+    assert res.binary_score == "no"
